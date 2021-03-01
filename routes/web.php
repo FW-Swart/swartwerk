910547868a18efc17ab22050a75b0101f1b30fad
FW-Swart <folkert@swartwerk.nl>
2021-03-01 20:26:05 +0100

    setup part2 - Breeze/folderstructure

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+|PUBLIC GROUP
+|--------------------------------------------------------------------------
+*/
+
+// Test Views
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Main Views
+Route::get('/enter', function () {
+    return view('main/enter');
+});
+
+Route::get('/home', function () {
+    return view('main/home');
+});
+
+/*
+|--------------------------------------------------------------------------
+|ADMIN GROUP
+|--------------------------------------------------------------------------
+*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
